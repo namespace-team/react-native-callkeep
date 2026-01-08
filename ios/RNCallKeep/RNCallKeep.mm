71832f8fac40fc2cb952b0601d037cd3d041e513
Naresh Rawat <nareshrawat33@gmail.com>
2026-01-08 10:36:42 +0545

    fix header import

--- ios/RNCallKeep/RNCallKeep.mm
+++ ios/RNCallKeep/RNCallKeep.mm
@@ -16,7 +16,7 @@
 
 #import <AVFoundation/AVAudioSession.h>
 #import <CallKit/CallKit.h>
-#import "RNCallKeepSpec.h"
+#import <RNCallKeepSpec/RNCallKeepSpec.h>
 
 #ifdef DEBUG
 static int const OUTGOING_CALL_WAKEUP_DELAY = 10;
